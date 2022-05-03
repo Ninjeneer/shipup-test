2ff3719bab903b23ebd2ad8b0725407a2c5eba46
Loan Alouache <alouache.loan@gmail.com>
2022-05-03 18:55:58 +0200

    level3

--- level3/main.py
+++ level3/main.py
@@ -1,15 +1,19 @@
 import json
 from datetime import date, timedelta
+from typing import Tuple
 
 carriers = []
 packages = []
+country_distance = []
 output_data = { "deliveries": [] }
+
 
 # Read from input file
 with open("data/input.json", "r") as file:
     data = json.loads(file.read())
     carriers = data["carriers"]
     packages = data["packages"]
+    country_distance = data["country_distance"]
 
 
 
@@ -32,6 +36,14 @@ def update_arrival_date_with_weekends(shipping_date: date, arrival_date: date) -
                 arrival_date += timedelta(days=1)
     return arrival_date
 
+def add_oversea_delays(arrival_date: date, origin: str, dest: str, threshold: int) -> Tuple[date, int]:
+    """
+    Calculate the delay added by oversea distance
+    """
+    distance = country_distance[origin][dest]
+    oversea_delay = distance // threshold
+    return arrival_date + timedelta(days=oversea_delay), oversea_delay
+
 
 for package in packages:
     # Get the associated carrier
@@ -42,12 +54,17 @@ for package in packages:
     # Add the carrier delays to the shipping date
     shipping_date = date(splitted_shipping_date[0], splitted_shipping_date[1], splitted_shipping_date[2])
     arrival_date = shipping_date + timedelta(days=associated_carrier['delivery_promise'] + 1)
+    
     # Add the weekend delays to the arrival date
     arrival_date = update_arrival_date_with_weekends(shipping_date, arrival_date)
 
+    # Add distance delays
+    arrival_date, oversea_delay = add_oversea_delays(arrival_date,package["origin_country"], package["destination_country"], associated_carrier["oversea_delay_threshold"])    
+
     output_data["deliveries"].append({
         "package_id": package["id"],
-        "expected_delivery": str(arrival_date)
+        "expected_delivery": str(arrival_date),
+        "oversea_delay": oversea_delay
     })
 
 print(output_data)
